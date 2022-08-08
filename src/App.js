ea24ae5ff8b1e0cce057458a7b6d0af98af945f4
kanakweb <kanakwebdev@gmail.com>
2022-08-08 12:14:24 +0600

    added navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Footer from './Pages/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Services from './Pages/Services/Services';
 
 function App() {
   return (
-    <div className="text-slate-900 ">
-    <h2>Hi this is kanak</h2>
-    </div>
+    <>
+      <Navbar>
+    <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='services' element={<Services></Services>}></Route>
+        <Route path='contact' element={<Contact></Contact>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
+        <Route path='*' element={<Login></Login>}></Route>
+
+    </Routes>
+      </Navbar>
+      <Footer/>
+    </>
   );
 }
 
