74e9684e4af9e4752eb3384c42ea07819d5c982f
kanakweb <kanakwebdev@gmail.com>
2022-08-10 12:30:42 +0600

    added new icon title build error

--- src/Images/Slider/Slider.js
+++ src/Images/Slider/Slider.js
@@ -3,7 +3,7 @@ import baner from "../Slider/SliderImages/1.png";
 
 const Slider = () => {
 	return (
-		<div class="carousel carousel-center max-w p-4 space-x-4 bg-neutral rounded-box">
+		<div class="carousel carousel-center max-w p-4 m-1 my-5 space-x-4 bg-neutral rounded-box">
   <div class="carousel-item">
     <img src="https://placeimg.com/250/180/arch" class="rounded-box" />
   </div> 
