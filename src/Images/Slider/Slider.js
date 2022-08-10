c73494e0e68f06df1599e4760baa3bf8ab8c72aa
kanakweb <kanakwebdev@gmail.com>
2022-08-10 13:42:50 +0600

    added a some content

--- src/Images/Slider/Slider.js
+++ src/Images/Slider/Slider.js
@@ -3,7 +3,8 @@ import baner from "../Slider/SliderImages/1.png";
 
 const Slider = () => {
 	return (
-		<div class="carousel carousel-center max-w p-4 m-1 my-5 space-x-4 bg-neutral rounded-box">
+		<marquee>
+		<div class="carousel carousel-center max-w p-4 m-1 my-5 space-x-4 rounded-box">
   <div class="carousel-item">
     <img src="https://placeimg.com/250/180/arch" class="rounded-box" />
   </div> 
@@ -26,6 +27,7 @@ const Slider = () => {
     <img src="https://placeimg.com/250/180/arch" class="rounded-box" />
   </div>
 </div>
+		</marquee>
 	);
 };
 
